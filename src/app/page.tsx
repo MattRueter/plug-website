f44475ea74e07e3590418c078e37b5309b793f45
Matt <84277551+MattRueter@users.noreply.github.com>
2025-05-20 11:53:16 -0500

    add banner image and favicon to app.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,100 +3,71 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">        
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          src={"/plug-hero-image.svg"}
+          alt="Plug and socket image with title Plug"
+          height={500}
+          width={500}
+          priority={true}
+          className="w-2/3"
         />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+       <h1>Plug</h1>
+       <span>
+        Real APIs. Zero Setup. Just Plug In. 
+        A growing collection of local-first APIs
+        for frontend devs to practice with real
+        backends—without boilerplate.
+       </span>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+      <h2>What is Plug?</h2>
+      <ul>
+        <li>Simple to intermediate APIs</li>
+        <li>Runs locally</li>
+        <li>No clooud setup required for most (though some require it)</li>
+        <li>Built for frontend devs to get practice integrating clients with apis</li>
+      </ul>
+
+      <h2>Why use Plug?</h2>
+      <ul>
+        <li>Easy set up</li>
+        <li>Real CRUD patterns</li>
+        <li>Resettable state</li>
+        <li>When you are ready you can refactor scale the api</li>
+        <li>Level up with Supabase, Mongo, etc...</li>
+      </ul>
+
+      <h2>Available APIs</h2>
+      <p>Coming soon</p>
+      <ul>
+        <li>Note taker</li>
+        <li>book reservation</li>
+        <li>cinema reservation</li>
+      </ul>
+
+      <h2>How to use Plug?</h2>
+      <span>
+        In most cases you can simply clone the repo you want, 
+        run the install script and start the api. 
+        Your job is to build the front end however you want. 
+        More advanced apis may require authentication, creating cloud service account and .env variables.
+      </span>
+
+      <h2>Whos is this for?</h2>
+      <ul>
+        <li>People learning front end development</li>
+        <li>Bootcamp students</li>
+      </ul>
+      <span>Anyone who wants to go deeper on the front end with api integration, state management etc... 
+        but hasn't yet learned to create backends or wants a ready made api to practice with.
+        This allows you to get away from 'fake data' json or js files without setting up cloud services (like Supabase, Firebase, etc..) or 
+        creating your own APIs. Of course you should eventually do those things!
+      </span>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <p>© Plug since 2025</p>
+      <p>gihub link here</p>
+      <p>license here</p>
       </footer>
     </div>
   );
